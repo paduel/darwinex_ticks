14c92a5a80e4e2b6390a3626204fb4d6c368acc7
paduel <paduel@gmail.com>
2018-11-12 18:17:17 +0100

    v.0.0.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except FileNotFoundError:
 setup(
     name='Darwinex-ticks',
     version='0.0.4',
-    packages=['pandas'],
+    packages=['darwinex_ticks'],
     url='https://github.com/paduel/darwinex_ticks',
     license='MIT',
     author='Paduel',
@@ -25,13 +25,4 @@ setup(
     long_description_content_type='text/markdown',
     python_requires=['pandas'],
     extras_require={'progress bar': ['ipywidgets']},
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-    ],
-
 )
