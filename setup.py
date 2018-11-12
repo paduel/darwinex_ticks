d8d3d45727bacb3c9c0b6b45ea51b3b72048c364
paduel <paduel@gmail.com>
2018-11-12 18:11:22 +0100

    v.0.0.4

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
 
 )
